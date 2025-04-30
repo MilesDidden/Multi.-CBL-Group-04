d3b494e422d87c1ae05f3bf4cac422f8bbad60f5
MilesDidden <132138911+MilesDidden@users.noreply.github.com>
2025-04-30 12:19:21 +0200

    Update DB_setup.py

--- DB_setup.py
+++ DB_setup.py
@@ -51,44 +51,45 @@ def parse_kml_multipolygon(parent_path: str, kml_file: str) -> pd.DataFrame:
     return df
 
 
+def list_all_crime_csv_files(parent_path: str= "data/crime_data/") -> list[str]:
+    
+    csv_paths = []
+    
+    for dirpath, dirnames, filenames in os.walk(parent_path):
+        for file in filenames:
+            if file.endswith(".csv"):
+                csv_paths.append(os.path.join(dirpath, file))
+
+    return csv_paths
+
+
 class DBhandler:
 
-    def __init__(self) -> None:
+    def __init__(self, db_loc: str= 'data/', db_name: str= 'crime_data_UK.db') -> None:
         
-        self.con = None
-        self.db_loc = None
-        self.db_name = None
-        self.db_path = None
-
-
-    # Create database
-    def init_db(self, db_loc: str="data/", db_name: str="crime_data_UK.db") -> None:
-
-        if (not os.path.exists(db_loc)) or (not db_loc.endswith("/")):
-            raise ValueError("Invalid folder location!\n" +
-                             "Make sure the database location ends with '/'")
-
-        if (not db_name.endswith(".db")):
-            raise ValueError("Invalid database name! Make sure the name ends with '.db'")
-
         self.db_loc = db_loc
         self.db_name = db_name
-        self.db_path = os.path.join(self.db_loc, self.db_name)
+        self.db_path = os.path.join(db_loc, db_name)
 
-        if os.path.exists(self.db_path):
-            print("\nDatabase already exists! Skipping this step!\n")
+        if not os.path.exists(self.db_path):
+            print("\nDatabase not found! Creating new database ...\n")
+
+            try:
+                self.con = sqlite3.connect(self.db_path)
+                print(f"\nDatabase created at {self.db_path}\n")
+            except:
+                raise ValueError("\nInvalid database location!\n")
+            
         else:
-            conn = sqlite3.connect(self.db_path)
-
-            print(f"\nDatabase created at {self.db_path}\n")
-
-            conn.close()
+            self.con = sqlite3.connect(self.db_path)
+            
+            print("\nEstablished connection with database!\n")
 
 
     # Connect to db to update tables
     def open_connection_db(self) -> None:
         if self.con is not None:
-            raise ValueError("Connection is open!")
+            print("\nConnection is already open!\n")
         
         if self.db_path is None:
             raise ValueError("Make sure the database location is correct!")
@@ -99,10 +100,12 @@ class DBhandler:
     # Close connection to db
     def close_connection_db(self) -> None:
         if self.con is None:
-            raise ValueError("No current open connections!")
+            raise ValueError("\nNo current open connections!\n")
         
         self.con.close()
         self.con = None
+
+        print('\nConnection successfully closed!\n')
 
 
     # Create table
@@ -141,7 +144,7 @@ class DBhandler:
         print(f"\nTable '{table_name}' deleted successfully (if it existed).\n")
 
 
-    # List tables
+    # List existing tables
     def list_tables(self) -> list:
         if self.con is None:
             raise ValueError("No active database connection. Open the connection first.")
@@ -185,24 +188,24 @@ class DBhandler:
         print(f"\nInserted {len(data)} rows into '{table_name}' successfully.\n")
 
 
+    # Remove duplicate rows
+    def remove_duplicate_rows(self) -> None:
+        pass
+
 
 if __name__ == "__main__":
-    db_handler = DBhandler()
+    db_handler = DBhandler(db_loc="data/", db_name="crime_data_UK.db")
 
-    db_handler.init_db(db_loc="data/", db_name="crime_data_UK.db")
-
-    db_handler.open_connection_db()
-
-    # Create table force_districts
-    db_handler.create_table(
-        table_name='force_districts',
-        columns={'force_district_name': 'TEXT PRIMARY KEY',
-                 'multipolygon':'TEXT'
-                 }
-    )
+    # # Create table force_districts
+    # db_handler.create_table(
+    #     table_name='force_districts',
+    #     columns={'force_district_name': 'TEXT PRIMARY KEY',
+    #              'multipolygon':'TEXT'
+    #              }
+    # )
 
     # # Preprocess data for force_districts
-    # path_to_district_kml_files = "data/force kmls/"
+    # path_to_district_kml_files = "data/force_kmls/"
     # list_of_district_kmls = os.listdir(path_to_district_kml_files)
 
     # df_polygons_of_districts = []
@@ -218,24 +221,21 @@ if __name__ == "__main__":
     #     data=df_districts.to_dict(orient='records')
     # )
 
-    # Create table crime data
-    db_handler.create_table(
-        table_name='crime',
-        columns={'db_id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
-                 'crime_id':'TEXT',
-                 'month':'TEXT',
-                 'reported_by':'TEXT',
-                 'falls_within':'TEXT',
-                 'long':'REAL',
-                 'lat':'REAL',
-                 'location':'TEXT',
-                 'lsoa_code':'TEXT',
-                 'lsoa_name':'TEXT',
-                 'crime_type':'TEXT',
-                 'last_outcome_category':'TEXT',
-                 'context':'TEXT'
-                 }
-    )
-
+    # # Create table crime data
+    # db_handler.create_table(
+    #     table_name='crime',
+    #     columns={'crime_id':'TEXT PRIMARY KEY',
+    #              'month':'TEXT',
+    #              'reported_by':'TEXT',
+    #              'falls_within':'TEXT',
+    #              'long':'REAL',
+    #              'lat':'REAL',
+    #              'location':'TEXT',
+    #              'lsoa_code':'TEXT',
+    #              'lsoa_name':'TEXT',
+    #              'crime_type':'TEXT',
+    #              'last_outcome_category':'TEXT'
+    #              }
+    # )
 
     db_handler.close_connection_db()
