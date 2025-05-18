57aa832b2ed6c32f2385452c21a632e4b98dec3a
MilesDidden <132138911+MilesDidden@users.noreply.github.com>
2025-05-18 22:06:37 +0200

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -97,9 +97,15 @@ if __name__ == "__main__":
     crime_sample = crime_df.sample(n=1000, random_state=42).copy()
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 # Step 2: Match each crime to a ward (trying for 1000 samples for now)
 print("Matching 1000 sampled crimes to wards...")
 crime_sample[['ward_code', 'ward_name']] = crime_sample['point'].apply(lambda pt: find_ward(pt, ward_df))
+=======
+    # Step 2: Match each crime to a ward (only 1000 records now!)
+    print("Matching 1000 sampled crimes to wards...")
+    crime_sample[['ward_code', 'ward_name']] = crime_sample['point'].apply(lambda pt: find_ward(pt, ward_df))
+>>>>>>> Stashed changes
 =======
     # Step 2: Match each crime to a ward (only 1000 records now!)
     print("Matching 1000 sampled crimes to wards...")
@@ -110,9 +116,15 @@ crime_sample[['ward_code', 'ward_name']] = crime_sample['point'].apply(lambda pt
     print(crime_sample[['crime_id', 'lat', 'long', 'ward_code', 'ward_name']])
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 # Step 4: Export
 csv_path = "data/crime_with_wards_sample1000.csv"
 crime_sample.to_csv(csv_path, index=False)
+=======
+    # Step 4: Export only the sample
+    csv_path = "data/crime_with_wards_sample1000.csv"
+    crime_sample.to_csv(csv_path, index=False)
+>>>>>>> Stashed changes
 =======
     # Step 4: Export only the sample
     csv_path = "data/crime_with_wards_sample1000.csv"
